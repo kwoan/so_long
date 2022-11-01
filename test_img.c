24919d067b2a1d67c7e2ca59010a690cf3d09691
Kwoanyoung Park <kwpark@c1r9s7.42seoul.kr>
2022-11-01 17:12:47 +0900

    img set.. to do size matches

--- test_img.c
+++ test_img.c
@@ -3,30 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   test_img.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kwpark <kwpark@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: kwpark <kwpark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 18:00:30 by kwpark            #+#    #+#             */
-/*   Updated: 2022/11/01 15:11:54 by kwpark           ###   ########.fr       */
+/*   Updated: 2022/11/01 17:10:10 by kwpark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minilibx/mlx.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include "so_long.h"
 
 int	main(void)
 {
 	void	*mlx;
 	void	*win;
-	void	*img1;
+	void	*player;
+	void	*door;
+	void	*fence;
+	void	*chest;
+	void	*plain;
 
 	int img_width;
 	int img_height;
 
 	mlx = mlx_init();
 	win = mlx_new_window(mlx, 1000, 500, "mlx 42");
-	img1 = mlx_xpm_file_to_image(mlx, "collectible.xpm", &img_width, &img_height);
-	mlx_put_image_to_window(mlx, win, img1, 0, 0);
+	player = mlx_xpm_file_to_image(mlx, "player.xpm", &img_width, &img_height);
+	door = mlx_xpm_file_to_image(mlx, "door.xpm", &img_width, &img_height);
+	fence = mlx_xpm_file_to_image(mlx, "fence.xpm", &img_width, &img_height);
+	chest = mlx_xpm_file_to_image(mlx, "chest.xpm", &img_width, &img_height);
+	plain = mlx_xpm_file_to_image(mlx, "plain.xpm", &img_width, &img_height);
+	mlx_put_image_to_window(mlx, win, player, 0, 0);
+	mlx_put_image_to_window(mlx, win, door, 64, 64);
+	mlx_put_image_to_window(mlx, win, fence, 128, 128);
+	mlx_put_image_to_window(mlx, win, chest, 192, 192);
+	mlx_put_image_to_window(mlx, win, plain, 256, 256);
 
 	mlx_loop(mlx);
 }
